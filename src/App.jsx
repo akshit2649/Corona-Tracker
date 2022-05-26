8ca320e2a422cf2d76571f5655325074b23620f9
Akshit Thakur <akshit2649@gmail.com>
2022-05-26 16:11:25 +0530

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const App = () => {
-  return <div>App</div>;
-};
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
 
+import styles from "./App.module.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+}
 export default App;
