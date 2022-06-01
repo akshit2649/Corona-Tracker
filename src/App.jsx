e14d265cdd4ea65309f33619db807e767187d2f7
Akshit Thakur <akshit2649@gmail.com>
2022-06-01 21:04:23 +0530

    Added the infect grid

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,27 @@ import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
 
+import { fetchData } from "./api";
+
 import styles from "./App.module.css";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
